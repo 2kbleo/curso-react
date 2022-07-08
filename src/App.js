0f6e84d4f1368393280a1216644a7c4fd008c98a
2kbleo <102769498+2kbleo@users.noreply.github.com>
2022-07-08 12:34:36 -0300

    propiedades

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-//import logo from './logo.svg';
 import './App.css';
-
-
-
+import React, {useState} from 'react';
 
 function LeoHola(props){
   return (
     <div>
       <td>
+        Numero 
       <h2>{props.propiedad1}</h2>
        <h3>{props.propiedad}</h3>
        </td>
       </div>
-      
-  )
-
+        )
+}
+function  Numero(){
+  const [cuenta, setDolar] = useState(10);
+  return (
+    <div>
+      <td>
+      <button onClick={() => setDolar(cuenta + 10)}>aumenta el dolar</button>
+      <h1>{cuenta}</h1>
+      </td>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-     <div>
+    <div>
     <h1>Propiedades</h1> 
+
     <LeoHola propiedad="uno" propiedad1="pepito" /> 
-    <LeoHola propiedad="dos" />
-    <LeoHola propiedad="tres" />
-    <LeoHola propiedad="cuatro" />
-    <LeoHola />
+    <LeoHola propiedad="dos" propiedad1="tiyulo3" />
+    <LeoHola propiedad="tres" propiedad1="pe312pito"/>
+    <LeoHola propiedad="cuatro" propiedad1="pepi321312to" />
+    <Numero />
      </div>
     </div>
   );
